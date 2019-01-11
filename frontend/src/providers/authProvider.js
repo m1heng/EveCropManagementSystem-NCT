f09cec4ca2ebaf1614668e943cfbcf59ac2dc2af
m1heng <miheng@me.com>
2019-01-11 11:52:49 +0800

    partial

--- frontend/src/providers/authProvider.js
+++ frontend/src/providers/authProvider.js
@@ -24,39 +24,55 @@ function login(credentials) {
             localStorage.setItem("token", token);
             localStorage.setItem("pid", decodedToken.pid);
             localStorage.setItem("role", decodedToken.role);
+            localStorage.setItem("exp", decodedToken.exp);
         });
+}
+
+function isNotExpired() {
+    const exp_time = new Date(0);
+    exp_time.setUTCSeconds(localStorage.getItem("exp"));
+    const cur_time = new Date();
+    return exp_time > cur_time;
 }
 
 function logout() {
     localStorage.removeItem("token");
     localStorage.removeItem("pid");
     localStorage.removeItem("role");
+    localStorage.removeItem("exp");
     window.location.reload();
 }
 
+function isLoggedIn() {
+    if (!isNotExpired()) {
+        logout();
+    }
+}
+
 function isDirector() {
+    console.log(isNotExpired());
     const current_role = localStorage.getItem("role");
-    return current_role.includes("director") ? true : false;
+    return current_role.includes("director");
 }
 
 function isAdmin() {
     const current_role = localStorage.getItem("role");
-    return current_role.includes("admin") ? true : false;
+    return current_role.includes("admin");
 }
 
 function isFC() {
     const current_role = localStorage.getItem("role");
-    return current_role.includes("fc") ? true : false;
+    return current_role.includes("fc");
 }
 
 function isHR() {
     const current_role = localStorage.getItem("role");
-    return current_role.includes("hr") ? true : false;
+    return current_role.includes("hr");
 }
 
 function isMember() {
     const current_role = localStorage.getItem("role");
-    return current_role.includes("member") ? true : false;
+    return current_role.includes("member");
 }
 
 export const authProvider = {
@@ -66,5 +82,6 @@ export const authProvider = {
     isAdmin,
     isFC,
     isHR,
-    isMember
+    isMember,
+    isNotExpired
 };
